33fa5b9b70b2b80ddc86bb046e04f0afe6f72c26
Erin  Rose <errose10@gmail.com>
2020-01-06 16:11:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -103,7 +103,7 @@ class TicTacToe
         end
 
         if won?
-          puts "Congratulations #{winner(board)}!"
+          puts "Congratulations #{winner}!"
         else draw?
           puts "Cat's Game!"
         end
